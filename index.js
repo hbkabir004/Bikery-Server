d9c7f11ab609d88c4eb1ac6cda542913eab7d053
ABIR <hbkabir004@gmail.com>
2022-11-26 15:24:20 +0600

    get user implemented

--- index.js
+++ index.js
@@ -53,6 +53,19 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/users', async (req, res) => {
+            const query = { };
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email};
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin : user?.role === 'admin'});
+        });
+
 
 
     }
