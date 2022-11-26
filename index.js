11bc5ad338a1b3524324c690824331a87844612b
ABIR <hbkabir004@gmail.com>
2022-11-26 15:23:09 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId, Collection } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const app = express();
 
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fkqwkjt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const verifyJWT = (req, res, next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.send(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+async function run() {
+    try {
+        const usersCollection = client.db('bikery').collection('users');
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+
+
+    }
+    finally {
+
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', async(req, res) => {
 res.send('Server is running in full swing')
